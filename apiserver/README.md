b3aa80c4d298580e9e13ad249470d5be80452eb0
Robert Hu <robert.hu@me.com>
2024-10-11 13:56:27 +0800

    add apiserver

---
+++ apiserver/README.md
@@ -0,0 +1,7 @@
+# debug environment
+VS Code : virtualenv
+Cmd + Shift + P : Python create terminal
+pip install --upgrade pip
+pip install virtualenv
+
+pip install netifaces
---
+++ apiserver/apiserver.py
@@ -0,0 +1,85 @@
+import asyncio
+from aiohttp import web
+from multidict import CIMultiDict
+import os, time
+import netifaces
+from os import system
+from datetime import datetime, timedelta, timezone 
+from dotenv import load_dotenv
+import logging
+import aiohttp_cors
+#from aiohttp_tokenauth import token_auth_middleware
+
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+async def getIpAddress(request: web.Request):
+    interface = os.getenv('INF')
+    #eth0 = netifaces.ifaddresses('eth0')[2][0]['addr']
+    eth0 = netifaces.ifaddresses(interface)[2][0]['addr']
+    mask = netifaces.ifaddresses(interface)[2][0]['netmask']
+    LOGGER.info('ethe0:%s', eth0)
+    #wlan0 = netifaces.ifaddresses('wlan0')[2][0]['addr']
+    inf ={
+        "ip": eth0,
+        "mask": mask
+    }
+    return web.json_response(inf, status=200, content_type='application/json')
+
+async def setIpAddress(request: web.Request):
+    inf = await request.json()
+    ipAddress = inf['ip']
+    interface = inf['interface']
+    LOGGER.info('ip:%s', ipAddress)
+    os.system(f'sudo ifconfig {interface} down')
+    os.system(f'sudo ifconfig {interface} {ipAddress}')
+    os.system(f'sudo ifconfig {interface} up')
+    return web.Response(status=200)
+
+#async def startupTasks(app: web.Application) -> None:
+#    app[SQL_CONN] = asyncio.create_task(initMySQL(app))
+#    app[REDIS_CONN] = asyncio.create_task(initReids(app))
+#    app[MQTT_CONN] = asyncio.create_task(initMQTT(app))
+
+def initServer() -> web.Application:
+    # Bearer Token
+    #async def ValidateToken(token: str):
+        # API 只給一個固定Token
+    #    LOGGER.info('validate api token => %s', token)
+    #    user = None
+    #    if token == os.getenv('API_TOKEN'):
+    #        user = {'uuid': 'ppm-api'}
+    #    return user
+
+    #app = web.Application(middlewares=[token_auth_middleware(ValidateToken)])
+    app = web.Application()
+    cors = aiohttp_cors.setup(app)
+
+    app.add_routes([web.post('/api/gateway/ip', setIpAddress),
+                    web.get('/api/gateway/ip', getIpAddress)
+                    
+                    ])
+
+    cors = aiohttp_cors.setup(app, defaults={
+                                        "*": aiohttp_cors.ResourceOptions(
+                                                allow_credentials=False,
+                                                expose_headers="*",
+                                                allow_headers="*"
+                                            )
+                                        })
+    for route in list(app.router.routes()):
+        cors.add(route)
+    
+    app.on_startup.append(startupTasks)
+    setup(app)
+    return app
+
+
+if __name__ == '__main__':
+    #asyncio.run(getIpAddress())
+    load_dotenv()
+    API_PORT = int(os.getenv('API_PORT'))
+    LOGGER.info('init api server port => %s', API_PORT)
+    web.run_app(initServer(), port=API_PORT)
